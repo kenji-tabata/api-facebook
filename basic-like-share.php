585dd6540b58ab104e6f55718a3b8ec599083900
Leandro Kenji <locky.kenji@gmail.com>
2016-09-21 11:57:00 -0300

    basic-like-share.php: Teste do SDK

---
+++ basic-like-share.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Facebook Like & Share JavaScript Example</title>
+        <meta charset="UTF-8">
+    </head>
+    <body>
+        <script>
+            window.fbAsyncInit = function () {
+                FB.init({
+                    appId: '1760820287509416',
+                    xfbml: true,
+                    version: 'v2.7'
+                });
+            };
+
+            (function (d, s, id) {
+                var js, fjs = d.getElementsByTagName(s)[0];
+                if (d.getElementById(id)) {
+                    return;
+                }
+                js = d.createElement(s);
+                js.id = id;
+                js.src = "//connect.facebook.net/en_US/sdk.js";
+                fjs.parentNode.insertBefore(js, fjs);
+            }(document, 'script', 'facebook-jssdk'));
+        </script>
+        <div
+            class="fb-like"
+            data-share="true"
+            data-width="450"
+            data-show-faces="true">
+        </div>
+    </body>
+</html>
