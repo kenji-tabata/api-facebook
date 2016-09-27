6bcdbf2e0c4b8deee8a024dc45a907f0c166dd24
Leandro Kenji <locky.kenji@gmail.com>
2016-09-27 12:56:03 -0300

    sdk-php/index.php: views

---
+++ sdk-php/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Facebook Login Example</title>
+        <meta charset="UTF-8">
+    </head>
+    <body>
+        <div id="navbar">
+            <?php include "login.php"; ?>
+        </div>
+        <h1>Home</h1>
+        <p>Página inicial</p>
+        <p><a href="outra.php">Outra página</a></p>
+    </body>
+</html>
---
+++ sdk-php/outra.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Facebook Login Example</title>
+        <meta charset="UTF-8">
+    </head>
+    <body>
+        <div id="navbar">
+            <?php include "login.php"; ?>
+        </div>
+        <h1>Home</h1>
+        <p><a href="index.php">Página inicial</a></p>
+        <p>Outra página</p>
+    </body>
+</html>
