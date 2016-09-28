8440b8c91990985c262b8b2aa4098d1f0d379110
Leandro Kenji <locky.kenji@gmail.com>
2016-09-28 13:00:14 -0300

    compartilhamento/perfil/index/php: Exemplo do compartilhamento da página

---
+++ compartilhamento/perfil/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Facebook Share JS</title>
+        <meta charset="UTF-8">
+        <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    </head>
+    <body>
+        <h1>Home</h1>
+        <p>Página inicial</p>
+        <button id="share">Compartilhar</button>
+        <script type="text/javascript" src="app.js"></script>
+        <script type='text/javascript'>
+            app.facebook.init();
+        </script>
+    </body>
+</html>
