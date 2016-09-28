c95fb8a1f75a0132f08322b713abd210546fe70b
Leandro Kenji <locky.kenji@gmail.com>
2016-09-28 11:47:47 -0300

    sdk-js-php/outra.php: view

--- sdk-js-php/outra.php
+++ sdk-js-php/outra.php
@@ -3,12 +3,19 @@
     <head>
         <title>Facebook Login Example</title>
         <meta charset="UTF-8">
+        <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
     </head>
     <body>
         <div id="navbar">
+            <?php include __DIR__ . '/login.php'; ?>
         </div>
         <h1>Home</h1>
         <p><a href="index.php">Página inicial</a></p>
         <p>Outra página</p>
+        <script type="text/javascript" src="js/app.js"></script>
+        <script type='text/javascript'>
+            app.facebook.init();
+            app.view_login.init();
+        </script>
     </body>
 </html>
