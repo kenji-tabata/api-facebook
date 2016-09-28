1b268677eb9bbde78c03989f0b3136caea816a68
Leandro Kenji <locky.kenji@gmail.com>
2016-09-28 11:46:06 -0300

    sdk-js-php/login.php: Correção da navbar login

--- sdk-js-php/login.php
+++ sdk-js-php/login.php
@@ -6,6 +6,9 @@ if (!isset($_SESSION['fb_access_token'])) {
     echo '<p><a href="#" id="btn-login"><img src="imagens/facebook.png" alt="Log in with Facebook!"/></a></p>';
 
 } else {
-    echo '<p><img style="vertical-align: middle;" src="<?php echo $usuario->getPicture()["url]; ?>" alt="foto">';
-    echo $usuario->getName() . ' - <a href="#" id="btn-logout">Sair</a></p>';
+    echo "<p><img style='vertical-align: middle;' src='" . $_SESSION["usuario"]["picture"]["data"]["url"] . "' alt='foto'>";
+    echo $_SESSION['usuario']['name'] . ' - <a href="#" id="btn-logout">Sair</a></p>';
 }
+?>
+
+<div id="notificacao"></div>
