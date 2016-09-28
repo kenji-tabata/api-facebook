a338d5e7ded844a6c04a3eb2e38292019af0b00e
Leandro Kenji <locky.kenji@gmail.com>
2016-09-28 11:42:16 -0300

    sdk-js-php/js/app.js: Adicionado login e logout

--- sdk-js-php/js/app.js
+++ sdk-js-php/js/app.js
@@ -10,14 +10,18 @@ app.facebook = {
         }
         js = d.createElement(s);
         js.id = id;
-        js.src = "//connect.facebook.net/en_US/sdk.js";
+        //js.src = "//connect.facebook.net/pt_BR/sdk/debug.js"; // Versão debug do SDK
+        js.src = "//connect.facebook.net/pt_BR/sdk.js"; // Versão minimizada do SDK
         fjs.parentNode.insertBefore(js, fjs);
     },
+    // Inicia a classe FB
     fbAsyncInit: function () {
         var me = this;
         window.fbAsyncInit = function () {
             FB.init({
                 appId: me.app_id,
+                xfbml: false, // Iniciar plugins sociais que utilizam XFBML
+                status: true,
                 cookie: true, // This is important, it's not enabled by default
                 version: 'v2.7'
             });
@@ -31,25 +35,66 @@ app.facebook = {
 
 app.view_login = {
     ctrl_login: $("#btn-login"),
+    ctrl_logout: $("#btn-logout"),
+    display: $("#notificacao"),
     init: function () {
         this.events();
     },
     events: function () {
         this.login();
+        this.logout();
     },
-    login: function() {
+    login: function () {
+        var that = this;
         this.ctrl_login.click(function () {
             FB.login(function (response) {
                 if (response.authResponse) {
-                    alert('You are logged in &amp; cookie set!');
-                    // Now you can redirect the user or do an AJAX request to
-                    // a PHP script that grabs the signed request from the cookie.
+                    $.get("login-callback.php", function (resp) {
+                        if (resp) {
+                            console.log(resp);
+                            window.location.href = resp;
+                        } else {
+                            console.log(resp);
+                            that.display.find('p').remove();
+                            that.display.append('<p>Login não foi autorizado!</p>');
+                        }
+                    });
                 } else {
-                    alert('User cancelled login or did not fully authorize.');
+                    that.display.find('p').remove();
+                    that.display.append('<p>Login foi cancelado!</p>');
                 }
             });
             return false;
         });
+    },
+    logout: function () {
+        var that = this;
+        that.ctrl_logout.click(function () {
+            // Verifica se o usuário está conectado no Facebook
+            FB.getLoginStatus(function (response) {
+                // Se estiver conectado...
+                if (response.status === 'connected') {
+                    // Efetua o logout no Facebook
+                    FB.logout(function (response) {
+                        console.log(response);
+                        // Efetua o logout no site
+                        $.get("logout-callback.php", function (resp) {
+                            if (resp) {
+                                console.log(resp);
+                                window.location.href = resp;
+                            } else {
+                                console.log(resp);
+                                that.display.find('p').remove();
+                                that.display.append('<p>Ocorreu um erro ao sair do sistema!</p>');
+                            }
+                        });
+                    });
+                } else {
+                    console.log(response);
+                    that.display.find('p').remove();
+                    that.display.append('<p>Você não esta autenticado no sistema!</p>');
+                }
+            });
+        });
     }
-
 };
