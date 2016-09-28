cf9301cd96e3f2869d509d5d27f717a212acc703
Leandro Kenji <locky.kenji@gmail.com>
2016-09-28 11:44:56 -0300

    sdk-js-php/login-callback.php: Retornando url após login e adicionando usuário na session

--- sdk-js-php/login-callback.php
+++ sdk-js-php/login-callback.php
@@ -2,32 +2,51 @@
 
 require_once __DIR__ . '/bootstrap.php';
 
-$fb     = getFB();
+$fb = getFB();
 $helper = $fb->getJavaScriptHelper();
 
 try {
-  $accessToken = $helper->getAccessToken();
-} catch(Facebook\Exceptions\FacebookResponseException $e) {
-  // When Graph returns an error
-  echo 'Graph returned an error: ' . $e->getMessage();
-  exit;
-} catch(Facebook\Exceptions\FacebookSDKException $e) {
-  // When validation fails or other local issues
-  echo 'Facebook SDK returned an error: ' . $e->getMessage();
-  exit;
+    $accessToken = $helper->getAccessToken();
+} catch (Facebook\Exceptions\FacebookResponseException $e) {
+    // When Graph returns an error
+    echo 'Graph returned an error: ' . $e->getMessage();
+    die();
+} catch (Facebook\Exceptions\FacebookSDKException $e) {
+    // When validation fails or other local issues
+    echo 'Facebook SDK returned an error: ' . $e->getMessage();
+    die();
 }
 
-if (! isset($accessToken)) {
-  echo 'No cookie set or no OAuth data could be obtained from cookie.';
-  exit;
+if (!isset($accessToken)) {
+    echo 'No cookie set or no OAuth data could be obtained from cookie.';
+    die();
 }
 
-// Logged in
-echo '<h3>Access Token</h3>';
-var_dump($accessToken->getValue());
+//var_dump($accessToken->getValue());
 
-$_SESSION['fb_access_token'] = (string) $accessToken;
 
-// User is logged in!
-// You can redirect them to a members-only page.
-//header('Location: https://example.com/members.php');
+if ($accessToken->getValue()) {
+    try {
+        // Solicita para o Facebook os dados do usuário que precisamos
+        $response = $fb->get('/me?fields=name,email,picture,gender', $accessToken->getValue());
+    } catch (Facebook\Exceptions\FacebookResponseException $e) {
+        // When Graph returns an error
+        echo 'Graph returned an error: ' . $e->getMessage();
+        die();
+    } catch (Facebook\Exceptions\FacebookSDKException $e) {
+        // When validation fails or other local issues
+        echo 'Facebook SDK returned an error: ' . $e->getMessage();
+        die();
+    }
+
+    // Adiciona na session os dados do usuário e o token do Facebook
+    $_SESSION['usuario'] = $response->getDecodedBody();
+    $_SESSION['fb_access_token'] = (string) $accessToken;
+
+    // Com a permissão do usuário salvamos os dados do usuário
+
+    // Retorna a url para onde o usuário será redirecionado após o login
+    echo App::getDominio() . '/outra.php';
+} else {
+    echo false;
+}
